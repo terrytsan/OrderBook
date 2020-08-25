20204362560c5adf029a6449d89ac1cc99cfcef0
Lydia <lydiaadejumo@outlook.com>
2020-08-25 22:22:53 +0100

    Compiled changes to app

--- OrderBookFrontEnd/orderbookapp/src/App.js
+++ OrderBookFrontEnd/orderbookapp/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 //Component Links
 import Navigation from "./components/navigation.jsx";
@@ -18,12 +18,12 @@ import OrderDetails from "./pages/orderDetails.jsx";
 class App extends Component {
   state = {
     selectedStock: {
-      id: 1,
-      stockExchangeId: 1,
-      symbol: "GOOG.L",
-      name: "Google (London)",
-      maxQuantity: 100,
-      tickSize: 1,
+      id: "",
+      stockExchangeId: "",
+      symbol: "",
+      name: "",
+      maxQuantity: "",
+      tickSize: "",
     },
     stocks: [
       {
@@ -50,8 +50,15 @@ class App extends Component {
         quantity: 200,
         price: "5.25",
         side: "BUY",
-        partyId: 1,
-        stockId: 1,
+        party: { id: 1, symbol: "CP1", name: "Counter Party 1" },
+        stock: {
+          id: 1,
+          stockExchangeId: 1,
+          symbol: "GOOG.L",
+          name: "Google (London)",
+          maxQuantity: 100,
+          tickSize: 1,
+        },
       },
       {
         id: 2,
@@ -59,27 +66,102 @@ class App extends Component {
         quantity: 150,
         price: "10.25",
         side: "SELL",
-        partyId: 2,
-        stockId: 1,
+        party: { id: 2, symbol: "CP2", name: "Counter Party 2" },
+        stock: {
+          id: 1,
+          stockExchangeId: 1,
+          symbol: "GOOG.L",
+          name: "Google (London)",
+          maxQuantity: 100,
+          tickSize: 1,
+        },
       },
     ],
-    currentOrderRecord: [
-      {
-        id: 1,
-        time: "5:00:34",
-        quantity: 200,
-        price: "5.25",
-        side: "BUY",
-        partyId: 1,
-        stockId: 1,
-      },
-    ],
+    currentOrderRecord: {},
     trades: [],
     counterParties: [
       { id: 1, symbol: "CP1", name: "Counter Party 1" },
       { id: 2, symbol: "CP2", name: "Counter Party 2" },
     ],
     types: ["BUY", "SELL"],
+    toOrderDetails: false,
+    newOrder: {
+      quantity: "",
+      price: "",
+      side: "BUY",
+      partyId: "1",
+      stockId: "1",
+    },
+  };
+
+  handleAddFormChange = (event) => {
+    // The event triggering this function should be an input's onChange event
+    // We need to grab the input's name & value so we can associate it with the
+    // newContactData within the App's state.
+    let inputName = event.target.name;
+    let inputValue = event.target.value;
+    let orderInfo = this.state.newOrder;
+
+    console.log(`Updating new contact data: ${inputName} : ${inputValue}`);
+
+    if (orderInfo.hasOwnProperty(inputName)) {
+      orderInfo[inputName] = inputValue;
+      this.setState({ newOrder: orderInfo });
+    }
+  };
+
+  handleUpdateFormChange = (event) => {
+    // The event triggering this function should be an input's onChange event
+    // We need to grab the input's name & value so we can associate it with the
+    // newContactData within the App's state.
+    let inputName = event.target.name;
+    let inputValue = event.target.value;
+    let orderInfo = this.state.newOrder;
+
+    console.log(`Updating new contact data: ${inputName} : ${inputValue}`);
+
+    if (orderInfo.hasOwnProperty(inputName)) {
+      orderInfo[inputName] = inputValue;
+      this.setState({ newOrder: orderInfo });
+    }
+  };
+
+  handleAddFormSubmit = (event) => {
+    console.log("Adding contact!");
+    if (event) {
+      event.preventDefault();
+    }
+    let orders = this.state.orders;
+    orders.push(this.state.newOrder);
+    this.setState({ orders });
+
+    /* THIS WILL LINK TO THE BACK-END!!!!
+    fetch(SERVICE_URL + "/contact/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state.newContactData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Add Contact - Success:", data);
+        this.setState({
+          newContactData: {
+            firstName: "",
+            lastName: "",
+            company: "",
+            phone: "",
+            email: "",
+          },
+        });
+        this.loadContactData();
+      })
+      .catch((error) => {
+        console.log("Add Contact - Error:");
+        console.log(error);
+      });
+      */
   };
 
   selectingStock = (stock) => {
@@ -90,10 +172,23 @@ class App extends Component {
   getAllOrderDetails = (orderId) => {
     //INCLUDE FETCH FROM THE CONTROLLER
     //SET CURRENTORDERRECORD
-    const currentOrderRecord = this.state.orders.find(
-      (order) => order.id === orderId
-    );
+    //IT TAKES A WHILE TO COMPLETE THE SET STATE FUNCTION - CHECK IT OUT!!!!!!!!!!!!!!!!!!!!!!
+    this.retrieveOrderDetails(orderId);
+
+    /*
+    this.setState(() => ({ toOrderDetails: true }));
+    if (this.state.toOrderDetails === true) {
+      return <Redirect to="/orderDetails" />;
+    }
+    */
   };
+
+  retrieveOrderDetails = (orderId) => {
+    const OrderRecord = this.state.orders.find((order) => order.id === orderId);
+    this.setState({ currentOrderRecord: OrderRecord });
+  };
+
+  componentDidUpdate() {}
 
   render() {
     return (
@@ -105,17 +200,22 @@ class App extends Component {
               path="/"
               render={(props) => (
                 <OrderBook
-                  {...props}
                   selectedStock={this.state.selectedStock}
                   stocks={this.state.stocks}
                   selectingStock={this.selectingStock}
                   trades={this.state.trades}
                   orders={this.state.orders}
+                  {...props}
                 />
               )}
               exact
             />
-            <Route path="/historicalTrades" component={HistoricalData} />
+            <Route
+              path="/pastTrades"
+              render={(props) => (
+                <HistoricalData {...props} trades={this.state.trades} />
+              )}
+            />
             <Route
               path="/addOrder"
               render={(props) => (
@@ -125,6 +225,9 @@ class App extends Component {
                   types={this.state.types}
                   stocks={this.state.stocks}
                   orders={this.state.orders}
+                  handleSubmit={this.handleAddFormSubmit}
+                  handleChange={this.handleAddFormChange}
+                  newOrder={this.state.newOrder}
                 />
               )}
             />
@@ -140,7 +243,13 @@ class App extends Component {
             />
             <Route
               path="/updateOrder"
-              render={(props) => <UpdateOrder {...props} />}
+              render={(props) => (
+                <UpdateOrder
+                  {...props}
+                  counterParties={this.state.counterParties}
+                  stocks={this.state.stocks}
+                />
+              )}
             />
             <Route
               path="/manageOrders"
