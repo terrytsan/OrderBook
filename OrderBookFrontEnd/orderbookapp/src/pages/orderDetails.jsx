eb53f7da65f3b4dc728953605eb2f04e2882b159
Lydia <lydiaadejumo@outlook.com>
2020-08-26 15:34:06 +0100

    removing irrelevant prop
    Removing prop from method (counterparties)

--- OrderBookFrontEnd/orderbookapp/src/pages/orderDetails.jsx
+++ OrderBookFrontEnd/orderbookapp/src/pages/orderDetails.jsx
@@ -5,14 +5,11 @@ import OrderHistoryTable from "../components/orderDetails/orderHistoryTable";
 class OrderDetails extends Component {
   state = {};
   render() {
-    const { orderRecord, counterParties } = this.props;
+    const { order, orderRecords } = this.props;
     return (
-      <div>
-        <h1>Order Details</h1>
-        <OrderHistoryTable
-          orders={orderRecord}
-          counterParties={counterParties}
-        />
+      <div className="container">
+        <h1 className="m-2">Order Details</h1>
+        <OrderHistoryTable orderRecords={orderRecords} />
       </div>
     );
   }
