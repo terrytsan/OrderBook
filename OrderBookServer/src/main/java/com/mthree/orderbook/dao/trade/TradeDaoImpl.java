e1727bd2490f47b9abb57950047bf7ee6cf29b32
William Roberts <billyjamesroberts@hotmail.com>
2020-08-26 11:11:35 +0100

    Completed Daos

--- OrderBookServer/src/main/java/com/mthree/orderbook/dao/trade/TradeDaoImpl.java
+++ OrderBookServer/src/main/java/com/mthree/orderbook/dao/trade/TradeDaoImpl.java
@@ -1,7 +1,13 @@
 package com.mthree.orderbook.dao.trade;
 
+import com.mthree.orderbook.dao.order.OrderDao;
 import com.mthree.orderbook.dao.order.OrderDaoImpl;
+import com.mthree.orderbook.dao.party.PartyDaoImpl;
+import com.mthree.orderbook.dao.stock.StockDaoImpl;
+import com.mthree.orderbook.dao.stockexchange.StockExchangeDaoImpl;
+import com.mthree.orderbook.entity.Order;
 import com.mthree.orderbook.entity.Side;
+import com.mthree.orderbook.entity.Stock;
 import com.mthree.orderbook.entity.Trade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,6 +17,7 @@ import org.springframework.stereotype.Repository;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -21,14 +28,17 @@ public class TradeDaoImpl implements TradeDao {
     @Autowired
     JdbcTemplate jdbc;
 
+
     @Override
     public List<Trade> getTrades(int stockExchangeId, int count) {
         final String GET_TRADES =
                 "SELECT * FROM trade " +
-                        "INNER JOIN order " +
-                        "ON trade.buyOrderId = order.id " +
+                        "INNER JOIN orderHistory " +
+                        "ON orderHistory.id = trade.buyOrderId " +
+                        "INNER JOIN `order` " +
+                        "ON orderHistory.orderId = `order`.id " +
                         "INNER JOIN stock " +
-                        "ON order.stockId = stock.id " +
+                        "ON `order`.stockId = stock.id " +
                         "WHERE stock.stockExchangeId = ?";
         List<Trade> trades = jdbc.query(GET_TRADES, new TradeMapper(), stockExchangeId);
 
@@ -47,9 +57,11 @@ public class TradeDaoImpl implements TradeDao {
     public List<Trade> getTradesForStock(int stockId) {
         final String GET_TRADES =
                 "SELECT * FROM trade " +
-                        "INNER JOIN order " +
-                        "ON trade.buyOrderId = order.id " +
-                        "WHERE order.stockId = ?";
+                        "INNER JOIN orderHistory " +
+                        "ON trade.buyOrderId = orderHistory.id " +
+                        "INNER JOIN `order` " +
+                        "ON orderHistory.orderId = `order`.id " +
+                        "WHERE `order`.stockId = ?";
         List<Trade> trades = jdbc.query(GET_TRADES, new TradeMapper(), stockId);
 
         for(Trade trade : trades) {
@@ -62,9 +74,11 @@ public class TradeDaoImpl implements TradeDao {
     public List<Trade> getTradesByOrderId(int orderId) {
         final String GET_TRADES =
                 "SELECT * FROM trade " +
-                        "WHERE buyOrderId = ? " +
-                        "OR sellOrderId = ? ";
-        List<Trade> trades = jdbc.query(GET_TRADES, new TradeMapper(), orderId, orderId);
+                        "INNER JOIN orderHistory " +
+                        "ON orderHistory.id = trade.buyOrderId " +
+                        "OR orderHistory.id = trade.sellOrderId " +
+                        "WHERE orderHistory.orderId = ?";
+        List<Trade> trades = jdbc.query(GET_TRADES, new TradeMapper(), orderId);
 
         for(Trade trade : trades) {
             assignBuyAndSellOrders(trade);
@@ -74,10 +88,19 @@ public class TradeDaoImpl implements TradeDao {
 
     @Override
     public void addTrade(Trade trade) {
+        final String GET_HISTORY_ID =
+                "SELECT orderHistory.id FROM orderHistory " +
+                        "INNER JOIN `order` " +
+                        "ON `order`.id = orderId " +
+                        "AND `order`.version = orderHistory.version " +
+                        "WHERE orderId = ?";
+        int buyOrder = jdbc.queryForObject(GET_HISTORY_ID, Integer.class, trade.getBuyOrder().getId());
+        int sellOrder = jdbc.queryForObject(GET_HISTORY_ID, Integer.class, trade.getSellOrder().getId());
+
         final String ADD_TRADE =
                 "INSERT INTO trade(buyOrderId, sellOrderId, quantity, price, timestamp) " +
                         "VALUES(?, ?, ?, ?, ?)";
-        jdbc.update(ADD_TRADE, trade.getBuyOrder().getId(), trade.getSellOrder().getId(), trade.getQuantity(),
+        jdbc.update(ADD_TRADE, buyOrder, sellOrder, trade.getQuantity(),
                 trade.getPrice(), trade.getTimestamp());
 
         trade.setId(jdbc.queryForObject("SELECT LAST_INSERT_ID()", Integer.class));
@@ -85,20 +108,55 @@ public class TradeDaoImpl implements TradeDao {
 
     private Trade assignBuyAndSellOrders(Trade trade) {
         final String GET_BUY =
-                "SELECT * FROM order " +
-                "INNER JOIN trade " +
-                        "ON trade.buyOrderId = order.id " +
+                "SELECT `order`.id, `order`.side, orderHistory.quantity, orderHistory.price, orderHistory.state, " +
+                        "orderHistory.version, orderHistory.timestamp FROM orderHistory " +
+                        "INNER JOIN `order` " +
+                        "ON `order`.id = orderHistory.orderId " +
+                        "INNER JOIN trade " +
+                        "ON trade.buyOrderId = orderHistory.id " +
                         "WHERE trade.id = ?";
-        trade.setBuyOrder(jdbc.queryForObject(GET_BUY, new OrderDaoImpl.OrderMapper(), trade.getId()));
+        Order buyOrder = jdbc.queryForObject(GET_BUY, new OrderDaoImpl.OrderMapper(), trade.getId());
+        trade.setBuyOrder(assignStockAndParty(buyOrder));
+
 
         final String GET_SELL =
-                "SELECT * FROM order " +
+                "SELECT `order`.id, `order`.side, orderHistory.quantity, orderHistory.price, orderHistory.state, " +
+                        "orderHistory.version, orderHistory.timestamp FROM orderHistory " +
+                        "INNER JOIN `order` " +
+                        "ON `order`.id = orderHistory.orderId " +
                         "INNER JOIN trade " +
-                        "ON trade.sellOrderId = order.id " +
+                        "ON trade.sellOrderId = orderHistory.id " +
                         "WHERE trade.id = ?";
-        trade.setSellOrder(jdbc.queryForObject(GET_SELL, new OrderDaoImpl.OrderMapper(), trade.getId()));
+        Order sellOrder = jdbc.queryForObject(GET_SELL, new OrderDaoImpl.OrderMapper(), trade.getId());
+        trade.setSellOrder(assignStockAndParty(sellOrder));
 
         return trade;
+    }
+
+    private Order assignStockAndParty(Order order) {
+        final String GET_STOCK =
+                "SELECT * FROM stock " +
+                        "INNER JOIN `order` " +
+                        "ON stock.id = `order`.stockId " +
+                        "WHERE `order`.id = ?";
+        Stock stock =  jdbc.queryForObject(GET_STOCK, new StockDaoImpl.StockMapper(), order.getId());
+
+        final String GET_STOCK_EXCHANGE =
+                "SELECT * FROM stockExchange " +
+                        "INNER JOIN stock ON stock.stockExchangeId = stockExchange.id " +
+                        "WHERE stock.id = ?";
+        stock.setStockExchange(jdbc.queryForObject(GET_STOCK_EXCHANGE,
+                new StockExchangeDaoImpl.StockExchangeMapper(), stock.getId()));
+
+        order.setStock(stock);
+
+        final String GET_PARTY =
+                "SELECT * FROM party " +
+                        "INNER JOIN `order` " +
+                        "ON `order`.partyId = party.id " +
+                        "WHERE `order`.id = ?";
+        order.setParty(jdbc.queryForObject(GET_PARTY, new PartyDaoImpl.PartyMapper(), order.getId()));
+        return order;
     }
 
     public static final class TradeMapper implements RowMapper<Trade> {
@@ -108,7 +166,8 @@ public class TradeDaoImpl implements TradeDao {
             trade.setId(rs.getInt("id"));
             trade.setQuantity(rs.getInt("quantity"));
             trade.setPrice(rs.getBigDecimal("price"));
-            trade.setTimestamp(LocalDateTime.parse(rs.getString("timestamp")));
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            trade.setTimestamp(LocalDateTime.parse(rs.getString("timestamp"), formatter));
             return trade;
         }
     }
--- OrderBookServer/src/test/java/com/mthree/orderbook/dao/trade/TradeDaoImplTest.java
+++ OrderBookServer/src/test/java/com/mthree/orderbook/dao/trade/TradeDaoImplTest.java
@@ -117,11 +117,139 @@ class TradeDaoImplTest {
         trade2.setSellOrder(order2);
         trade2.setQuantity(30);
         trade2.setPrice(order2.getPrice());
-        trade2.setTimestamp(LocalDateTime.now().minusHours(1));
+        trade2.setTimestamp(LocalDateTime.now().minusHours(1).withNano(0));
         tradeDao.addTrade(trade2);
 
         List<Trade> trades = tradeDao.getTrades(stockExchange.getId(), 2);
-        assertEquals(trade2, trades.get(0));
-        assertEquals(trade, trades.get(1));
+        assertEquals(trade, trades.get(0));
+        assertEquals(trade2, trades.get(1));
+    }
+
+    @Test
+    public void addGetByStock() {
+        StockExchange stockExchange = new StockExchange();
+        stockExchange.setName("TEST");
+        stockExchange.setCentralCounterParty("TESTING");
+        stockExchangeDao.addStockExchange(stockExchange);
+
+        Stock stock = new Stock();
+        stock.setStockExchange(stockExchange);
+        stock.setName("TEST STOCK");
+        stock.setSymbol("TEST");
+        stock.setMaxQuantity(100);
+        stock.setTickSize(new BigDecimal("0.01"));
+        stockDao.addStock(stock);
+
+        Party party = new Party();
+        party.setName("TEST PARTY");
+        party.setSymbol("TEST");
+        partyDao.addParty(party);
+
+        Order order = new Order();
+        order.setStock(stock);
+        order.setParty(party);
+        order.setSide(Side.BUY);
+        order.setQuantity(30);
+        order.setPrice(new BigDecimal("300"));
+        order.setState(State.LIVE);
+        order.setVersion(1);
+        order.setTimestamp(LocalDateTime.now().withNano(0));
+        orderDao.addOrder(order);
+
+        Order order2 = new Order();
+        order2.setStock(stock);
+        order2.setParty(party);
+        order2.setSide(Side.SELL);
+        order2.setQuantity(30);
+        order2.setPrice(new BigDecimal("300"));
+        order2.setState(State.LIVE);
+        order2.setVersion(1);
+        order2.setTimestamp(LocalDateTime.now().withNano(0));
+        orderDao.addOrder(order2);
+
+        Trade trade = new Trade();
+        trade.setBuyOrder(order);
+        trade.setSellOrder(order2);
+        trade.setQuantity(30);
+        trade.setPrice(order2.getPrice());
+        trade.setTimestamp(order2.getTimestamp());
+        tradeDao.addTrade(trade);
+
+        Trade trade2 = new Trade();
+        trade2.setBuyOrder(order);
+        trade2.setSellOrder(order2);
+        trade2.setQuantity(30);
+        trade2.setPrice(order2.getPrice());
+        trade2.setTimestamp(LocalDateTime.now().minusHours(1).withNano(0));
+        tradeDao.addTrade(trade2);
+
+        List<Trade> trades = tradeDao.getTradesForStock(stock.getId());
+
+        assertEquals(trade, trades.get(0));
+        assertEquals(trade2, trades.get(1));
+    }
+
+    @Test
+    public void addGetByOrder() {
+        StockExchange stockExchange = new StockExchange();
+        stockExchange.setName("TEST");
+        stockExchange.setCentralCounterParty("TESTING");
+        stockExchangeDao.addStockExchange(stockExchange);
+
+        Stock stock = new Stock();
+        stock.setStockExchange(stockExchange);
+        stock.setName("TEST STOCK");
+        stock.setSymbol("TEST");
+        stock.setMaxQuantity(100);
+        stock.setTickSize(new BigDecimal("0.01"));
+        stockDao.addStock(stock);
+
+        Party party = new Party();
+        party.setName("TEST PARTY");
+        party.setSymbol("TEST");
+        partyDao.addParty(party);
+
+        Order order = new Order();
+        order.setStock(stock);
+        order.setParty(party);
+        order.setSide(Side.BUY);
+        order.setQuantity(30);
+        order.setPrice(new BigDecimal("300"));
+        order.setState(State.LIVE);
+        order.setVersion(1);
+        order.setTimestamp(LocalDateTime.now().withNano(0));
+        orderDao.addOrder(order);
+
+        Order order2 = new Order();
+        order2.setStock(stock);
+        order2.setParty(party);
+        order2.setSide(Side.SELL);
+        order2.setQuantity(30);
+        order2.setPrice(new BigDecimal("300"));
+        order2.setState(State.LIVE);
+        order2.setVersion(1);
+        order2.setTimestamp(LocalDateTime.now().withNano(0));
+        orderDao.addOrder(order2);
+
+        Trade trade = new Trade();
+        trade.setBuyOrder(order);
+        trade.setSellOrder(order2);
+        trade.setQuantity(30);
+        trade.setPrice(order2.getPrice());
+        trade.setTimestamp(order2.getTimestamp());
+        tradeDao.addTrade(trade);
+
+        Trade trade2 = new Trade();
+        trade2.setBuyOrder(order);
+        trade2.setSellOrder(order2);
+        trade2.setQuantity(30);
+        trade2.setPrice(order2.getPrice());
+        trade2.setTimestamp(LocalDateTime.now().minusHours(1).withNano(0));
+        tradeDao.addTrade(trade2);
+
+        List<Trade> trades = tradeDao.getTradesByOrderId(order.getId());
+
+        assertEquals(trade, trades.get(0));
+        assertEquals(trade2, trades.get(1));
     }
 }
