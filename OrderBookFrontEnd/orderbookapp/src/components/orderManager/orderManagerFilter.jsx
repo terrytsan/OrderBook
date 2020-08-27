d421b74b0bb6430d6fafb8e473579e13d724febf
Lydia <lydiaadejumo@outlook.com>
2020-08-27 15:07:01 +0100

    New Filter button

---
+++ OrderBookFrontEnd/orderbookapp/src/components/orderManager/orderManagerFilter.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Select from "../input/select";
+import { Form } from "react-bootstrap";
+
+class FilterOrders extends Component {
+  state = {
+    states: [
+      "LIVE ORDERS",
+      "COMPLETED ORDERS",
+      "CANCELLED ORDERS",
+      "ALL ORDERS",
+    ],
+  };
+  render() {
+    let { handleFilterChange, selectedFilter } = this.props;
+    return (
+      <div className="container">
+        <Form>
+          <Form.Group>
+            <Form.Label>Filter Options:</Form.Label>
+            <Form.Control
+              as="select"
+              name="state"
+              onChange={handleFilterChange}
+            >
+              {this.state.states.map((state) => (
+                <option value={state}>{state}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default FilterOrders;
+
+/*<Select
+          title={"Filter Options:"}
+          name={"chosenFilter"}
+          value={selectedFilter}
+          placeholder={"Select Filter..."}
+          options={this.state.states}
+          
+        />*/
--- OrderBookFrontEnd/orderbookapp/src/pages/orderManager.jsx
+++ OrderBookFrontEnd/orderbookapp/src/pages/orderManager.jsx
@@ -2,10 +2,36 @@ import React, { Component } from "react";
 
 import Stock from "../components/stock.jsx";
 import AllOrdersManaged from "../components/orderManager/allOrdersManaged.jsx";
+import FilterOrders from "../components/orderManager/orderManagerFilter.jsx";
 
 class OrderManager extends Component {
-  state = {};
+  state = {
+    selectedFilter: "ALL ORDERS",
+    orders: [],
+  };
+
+  handleFilterChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ selectedFilter: event.target.value });
+    console.log(this.state.selectedFilter);
+  };
+
+  filterOrders = () => {
+    let orders = [];
+    if (this.state.selectedFilter == "ALL ORDERS") {
+      orders = this.props.orders;
+    } else if (this.state.selectedFilter == "LIVE ORDERS") {
+      orders = this.props.orders.filter((order) => order.state === "LIVE");
+    } else if (this.state.selectedFilter == "CANCELLED ORDERS") {
+      orders = this.props.orders.filter((order) => order.state === "CANCELLED");
+    } else if (this.state.selectedFilter == "COMPLETED ORDERS") {
+      orders = this.props.orders.filter((order) => order.state === "COMPLETED");
+    }
+    return orders;
+  };
+
   render() {
+    let displayOrders = this.filterOrders();
     const {
       selectedStock,
       stocks,
@@ -21,15 +47,25 @@ class OrderManager extends Component {
         <h1>Order Manager</h1>
         <div>
           <div className="row m-2">
-            <Stock
-              selectedStock={selectedStock}
-              stocks={stocks}
-              selectingStock={selectingStock}
-              trades={trades}
-            />
+            <div className="col-9 border">
+              <div className="row">
+                <Stock
+                  selectedStock={selectedStock}
+                  stocks={stocks}
+                  selectingStock={selectingStock}
+                  trades={trades}
+                />
+              </div>
+            </div>
+            <div className="col-3">
+              <FilterOrders
+                handleFilterChange={this.handleFilterChange}
+                selectedFilter={this.state.selectedFilter}
+              />
+            </div>
           </div>
           <AllOrdersManaged
-            orders={orders}
+            orders={displayOrders}
             getAllOrderDetails={getAllOrderDetails}
             retrieveOrderDetails={retrieveOrderDetails}
             handleCancelOrderClick={handleCancelOrderClick}
