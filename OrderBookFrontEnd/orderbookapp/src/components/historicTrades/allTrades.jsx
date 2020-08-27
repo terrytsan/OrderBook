6b11a60dd4f67d7f6aae4b7d1d90667a1d5927e7
Lydia <lydiaadejumo@outlook.com>
2020-08-27 12:14:42 +0100

    Bold table headers

--- OrderBookFrontEnd/orderbookapp/src/components/historicTrades/allTrades.jsx
+++ OrderBookFrontEnd/orderbookapp/src/components/historicTrades/allTrades.jsx
@@ -8,12 +8,12 @@ class AllTrades extends Component {
     return (
       <React.Fragment>
         <div className="row">
-          <div className="id col">Trade ID</div>
-          <div className="soldPrice col">Price</div>
-          <div className="soldAmount col">Quantity</div>
-          <div className="CCP col">Counter Party</div>
-          <div className="stock col">Stock Symbol</div>
-          <div className="timestamp col">Timestamp</div>
+          <div className="id header col">Trade ID</div>
+          <div className="soldPrice header col">Price</div>
+          <div className="soldAmount header col">Quantity</div>
+          <div className="CCP header col">Counter Party</div>
+          <div className="stock header col">Stock Symbol</div>
+          <div className="timestamp header col">Timestamp</div>
         </div>
         {trades.map((trade) => (
           <Trade key={trade.id} trade={trade} stockExchange={stockExchange} />
